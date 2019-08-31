4e1ab8e5836afe42a17a63b0fce84f82f05def9e
ashique-nawaz <ashique.mca.du.2015@gmail.com>
2019-08-31 19:21:06 +0530

    added directory shared_project_1, containing a blank controller class with no business logic

---
+++ shared_project_1/src/shared_project_1/Controller.java
@@ -0,0 +1,10 @@
+package shared_project_1;
+
+public class Controller {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This it controller class");
+	}
+
+}
